a9f21795df17dcb1b74fc42fb4f40cf056e8da28
Nikolay Budin <n.budin@yandex.ru>
2019-05-11 06:18:22 +0300

    rewrite gui part, now use curses

--- visualizer.py
+++ visualizer.py
@@ -4,6 +4,7 @@
 import time
 import sys
 import colorama
+import curses
 
 colorama.init()
 
@@ -34,7 +35,8 @@ def get_dir(act):
         return 0
     if (act == ">"):
         return 1
-    return 2
+    print("'%s' is not a correct direction" % act, file=sys.stderr)
+    exit(1)
 
 prog = open(name, "r")
 
@@ -48,10 +50,10 @@ nodes = set()
 
 c = 0
 for s in prog:
-    if (c == 0 and s[:-1].isdecimal()):
+    if c == 0 and s.strip().isdecimal():
         if (int(s) != 1):
-            print("Sorry, i can't run multi-tape machine")
-            exit(0)
+            print("Sorry, i can't run multi-tape machine", file=sys.stderr)
+            exit(1)
     elif (s.startswith("start:")):
         cur = s[s.find(":") + 1:].strip()
     elif (s.startswith("accept:")):
@@ -63,100 +65,185 @@ for s in prog:
     elif (s.strip() != ""):
         tmp = s.split()
         if (len(tmp) != 6):
-            print("Parse failed on line: \"" + s[:-1] + "\"")
-            exit(0)
-        fr = tmp[0].strip()
-        ch = tmp[1].strip()
-        if (tmp[2] != "->"):
-            print("Parse failed on line: \"" + s[:-1] + "\"")
-            exit(0)
-        to = tmp[3].strip()
-        new_ch = tmp[4].strip()
-        act = tmp[5].strip()
-        if (act == 2):
-            print("Parse failed on line: \"" + s[:-1] + "\"")
-            exit(0)
+            print("Parse failed on line: \"%s\"" % s.strip(), file=sys.stderr)
+            exit(1)
+        fr = tmp[0]
+        ch = tmp[1]
+        if tmp[2] != "->":
+            print("Parse failed on line: \"%s\"" % s.strip(), file=sys.stderr)
+            exit(1)
+        to = tmp[3]
+        new_ch = tmp[4]
+        act = tmp[5]
         graph[(fr, ch)] = [to, new_ch, get_dir(act)]
         nodes.add(fr)
     c += 1
 
 for p in graph:
-    if (graph[p][0] != reject and graph[p][0] != accept and not graph[p][0] in nodes):
-        print("Warning: There's an edge leading to node \"" + graph[p][0] + "\", but this node doesn't exist")
+    if graph[p][0] != reject and graph[p][0] != accept and not graph[p][0] in nodes:
+        print("Warning: There's an edge leading to node \"%s\", but there's no edge going from this node" % graph[p][0])
 
 inp = open(f_inp, "r")
 
-pos = 0
-tape_positive = inp.readline().split()
-if (len(tape_positive) == 0):
-    tape_positive.append(blank)
-tape_negative = []
+carriage_pos = 0
+input_data = inp.readline().split()
+tape = dict()
+
+for i in range(len(input_data)):
+    tape[i] = input_data[i]
 
 prev_l = 0
 
-cnt_steps = 0
-flag = True
-while (True):
-    
-    cur_s = " "
-    cur_symbol = ""
-    if (pos < 0):
-        cur_symbol = tape_negative[-pos - 1]
-    else:
-        cur_symbol = tape_positive[pos]
+try:
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.start_color()
+    curses.use_default_colors()
+    stdscr.nodelay(1)
+    height, width = stdscr.getmaxyx()
+    curses.init_pair(1, curses.COLOR_BLUE, -1)
 
-    if (flag):
-        sys.stdout.write("\r")
-        for i in range(len(tape_negative) - 1, -1, -1):
-            if (-i - 1 == pos):
-                cur_s += colorama.Fore.GREEN + colorama.Style.BRIGHT + tape_negative[i] + colorama.Style.RESET_ALL + " "
-            else:
-                cur_s += tape_negative[i] + " "
+    cnt_steps = 0
+    flag = True
 
-        for i in range(len(tape_positive)):
-            if (i == pos):
-                cur_s += colorama.Fore.GREEN + colorama.Style.BRIGHT + tape_positive[i] + colorama.Style.RESET_ALL + " "
-                
-            else:
-                cur_s += tape_positive[i] + " "
+    gap = 5
+    cells_to_show = width // 2
 
-        cur_s += "   current node: " + cur
+    lborder = carriage_pos - gap
+    rborder = lborder + cells_to_show
 
-        if (len(cur_s) < prev_l):
-            cur_s += " " * (prev_l - len(cur_s))
-        sys.stdout.write(cur_s)
+    outcome = ""
 
-        prev_l = len(cur_s)
+    while (True):
+        if carriage_pos - lborder < gap:
+            lborder = carriage_pos - gap
+            rborder = lborder + cells_to_show
+        elif rborder - carriage_pos < gap:
+            rborder = carriage_pos + gap
+            lborder = rborder - cells_to_show
 
-    if (cur == accept):
-        sys.stdout.write("\n" + colorama.Fore.GREEN + "Accepted" + colorama.Style.RESET_ALL + "\n")
-        break
-    if (cur == reject):
-        sys.stdout.write("\n" + colorama.Fore.RED + "Rejected" + colorama.Style.RESET_ALL + "\n")
-        break
+        to_show = []
 
-    if ((cur, cur_symbol) in graph):
+        for i in range(cells_to_show):
+            if not lborder + i in tape:
+                tape[lborder + i] = blank
+            to_show.append(tape[lborder + i])
+
+        stdscr.clear()
+        stdscr.addstr(height - 1, 0, "Press 'q' to interrupt, Press space to pause/resume")
+        stdscr.addstr(0, 0, " ".join(to_show))
+        stdscr.addstr(1, (carriage_pos - lborder) * 2, "^")
+        stdscr.addstr(2, (carriage_pos - lborder) * 2, str(carriage_pos))
+        stdscr.addstr(5, 0, "Current node: %s" % cur)
+
+        stdscr.refresh()
+
+        cur_symbol = tape[carriage_pos]
+        
+        if (cur == accept):
+            outcome = colorama.Fore.GREEN + "Accepted" + colorama.Style.RESET_ALL
+            break
+        if (cur == reject):
+            outcome = colorama.Fore.RED + "Rejected" + colorama.Style.RESET_ALL
+            break
+
+        if not (cur, cur_symbol) in graph:
+            outcome = colorama.Fore.RED + "Failed, No edge from %s by symbol %s, Rejected" % (cur, cur_symbol) + colorama.Style.RESET_ALL
+            cur = reject
+            break
+
+        was = float(time.time())
+        interupted = False
+        paused = False
+        while time.time() - was < delay or paused:
+            key = stdscr.getch()
+            if key == ord('q'):
+                outcome = colorama.Fore.YELLOW + "Interrupted by user" + colorama.Style.RESET_ALL
+                interupted = True
+                break
+
+            if key == ord(' '):
+                paused ^= 1
+                if paused:
+                    stdscr.addstr(7, 0, "Paused (press space to resume)", curses.color_pair(1))
+                else:
+                    stdscr.addstr(6, 0, " " * 30)
+
+                stdscr.refresh()
+
+                while stdscr.getch() == ord(' '):
+                    pass
+        
+        if interupted:
+            break
+
         cnt_steps += 1
         tmp = graph[(cur, cur_symbol)]
         cur = tmp[0]
-        if (pos < 0):
-            tape_negative[-pos - 1] = tmp[1]
-        else:
-            tape_positive[pos] = tmp[1]
-        pos += tmp[2]
+        tape[carriage_pos] = tmp[1]
+        carriage_pos += tmp[2]
 
-        if (pos < 0):
-            if (-pos - 1 == len(tape_negative)):
-                tape_negative.append(blank)
+
+    curses.endwin()
+
+    print("Final state of the tape:")
+    tape_output = []
+    for pos in tape:
+        tape_output.append((pos, tape[pos]))
+
+    tape_output.sort()
+    l = tape_output[0][0]
+    tape_output = list(map(lambda x : x[1], tape_output))
+
+    while len(tape_output) and tape_output[0] == '_' and l < carriage_pos:
+        tape_output = tape_output[1:]
+        l += 1
+
+    while len(tape_output) and tape_output[-1] == '_' and carriage_pos + 1 < l + len(tape_output):
+        tape_output = tape_output[:-1]
+
+    tape_output = ["_", "_"] + tape_output + ["_", "_"]
+    l -= 2
+
+    removed_from_left = False
+    removed_from_right = False
+
+    while len(tape_output) > cells_to_show:
+        if carriage_pos - l > l + len(tape_output) - carriage_pos:
+            if tape_output[0] != '_' and not removed_from_left:
+                removed_from_left = True
+                cells_to_show -= 2
+
+            tape_output = tape_output[1:]
+            l += 1
         else:
-            if (pos == len(tape_positive)):
-                tape_positive.append(blank)
+            if tape_output[-1] != '_' and not removed_from_right:
+                removed_from_right = True
+                cells_to_show -= 2
+
+            tape_output = tape_output[:-1]
+
+    if removed_from_left:
+        tape_output = ["..."] + tape_output
+
+    if removed_from_right:
+        tape_output = tape_output + ["..."]
+
+    print(" ".join(tape_output))
+
+    if not removed_from_left:
+        output_carriage_pos = (carriage_pos - l) * 2
     else:
-        sys.stdout.write("\n" + colorama.Fore.RED + "Failed, No edge by this state: (" + cur + ", " + cur_symbol + "), from current node, Rejected" + colorama.Style.RESET_ALL + "\n")
-        cur = reject
-        break
+        output_carriage_pos = (carriage_pos - l) * 2 + 4
 
-    time.sleep(delay)
+    print(" " * output_carriage_pos + "^")
 
-
-sys.stdout.write("Total steps: " + str(cnt_steps) + "\n")
+    print(outcome)
+    print("Total steps: %d" % cnt_steps)
+except KeyboardInterrupt as e:
+    pass
+except Exception as error:
+    curses.endwin()
+    print(error)
+    print("Something went wrong")
